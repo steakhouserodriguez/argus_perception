93a9fee7553fad04ac14bb8ef2d0f9bd0a78be47
Andrew Chen <andrewtheannihilator@gmail.com>
2013-02-12 11:29:00 -0800

    removed empty file plot.py

--- nodes/plot.py
+++
@@ -1,8 +0,0 @@
-
-import pyqtgraph as pg
-import numpy as np
-x = np.random.normal(size=1000)
-y = np.random.normal(size=1000)
-pg.plot(x, y, pen=None, symbol='o')  ## setting pen=None disables line drawing
-
-
